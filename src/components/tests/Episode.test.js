2aee1f67b32314a22716377b9bd5ee4c71d59c56
Dave Madden <davelearnsstuff@gmail.com>
2021-11-08 12:20:35 -0800

    episode testing complete

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,24 +8,41 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "what are people for?",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
-    //Add in approprate test data structure here.
+    id:1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: "what are people for?",
+    runtime: 1
 }
 
 test("renders without error", () => {
-
+    render(<Episode episode={testEpisode}/>)
 });
 
-test("renders the summury test passed as prop", ()=>{
+test("renders the summary test passed as prop", ()=>{
+    render(<Episode episode={testEpisode}/>)
+
+    const summaryText = screen.queryByText(/what are people for?/i);
+
+    expect(summaryText).toBeInTheDocument();
+    expect(summaryText).not.toBeNull();
+    expect(summaryText).toBeTruthy();
     
 });
 
 test("renders default image when image is not defined", ()=>{
-    
+    render(<Episode episode={testEpisodeWithoutImage}/>)
+
+    const defImg = screen.queryByAltText("./stranger_things.png");
+
+    expect(defImg).toBeInTheDocument();
 })
 
 //Tasks
